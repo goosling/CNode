aada0feeb08e5c4ee761cfc006549b7b234afd12
goosling <xiaoliu920@163.com>
2016-08-13 13:56:46 +0800

    layout

---
+++ app/src/main/java/joe/com/cnode/ui/view/IBackToContentTopView.java
@@ -0,0 +1,9 @@
+package joe.com.cnode.ui.view;
+
+/**
+ * Created by JOE on 2016/8/12.
+ */
+public interface IBackToContentTopView {
+
+    void backToContentTop();
+}
---
+++ app/src/main/java/joe/com/cnode/ui/widget/TopicWebView.java
@@ -0,0 +1,11 @@
+package joe.com.cnode.ui.widget;
+
+import joe.com.cnode.ui.view.IBackToContentTopView;
+
+/**
+ * Created by JOE on 2016/8/12.
+ */
+public class TopicWebView extends CNodeWebView implements IBackToContentTopView {
+
+
+}
